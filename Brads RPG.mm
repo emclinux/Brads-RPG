9cd37b0ad24176f47c0cd94bee2778711a3051a8
Brad Martin <emclinux@gmail.com>
2011-11-30 09:45:15 -0500

    Version 0.0.4 See CHANGELOG

---
+++ Brads RPG.mm
@@ -0,0 +1,55 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1322586799990" ID="ID_1858773832" MODIFIED="1322587206645" TEXT="Brads RPG">
+<node CREATED="1322587053095" HGAP="48" ID="ID_483674979" MODIFIED="1322624310811" POSITION="right" TEXT="Player/Monster" VSHIFT="-103"/>
+<node CREATED="1322587068647" HGAP="63" ID="ID_1904500263" MODIFIED="1322624705711" POSITION="left" TEXT="Player Only" VSHIFT="-125">
+<node CREATED="1322624360193" ID="ID_232906715" MODIFIED="1322624371149" TEXT="Player Stats"/>
+<node CREATED="1322624375419" ID="ID_466784525" MODIFIED="1322624396327" TEXT="Equipt item"/>
+<node CREATED="1322624399824" ID="ID_575657260" MODIFIED="1322624407115" TEXT="Use item"/>
+<node CREATED="1322624412792" ID="ID_447185541" MODIFIED="1322624433426" TEXT="Experance and leveling">
+<node CREATED="1322624439702" ID="ID_129050303" MODIFIED="1322624477688" TEXT="Need to create and edit player stats"/>
+</node>
+<node CREATED="1322624707830" ID="ID_126953584" MODIFIED="1322624737512" TEXT="Player death should act like WoW (Sorry bliz)"/>
+</node>
+<node CREATED="1322587086052" HGAP="227" ID="ID_1636630969" MODIFIED="1322624305213" POSITION="right" TEXT="General Function" VSHIFT="-52">
+<node CREATED="1322624327238" ID="ID_817467847" MODIFIED="1322624353346" TEXT="Restart/Save game (0.0.4)"/>
+</node>
+<node CREATED="1322587099602" HGAP="166" ID="ID_1573117117" MODIFIED="1322624316286" POSITION="left" TEXT="Items/Inventory" VSHIFT="4">
+<node CREATED="1322624280715" ID="ID_602677295" MODIFIED="1322624658107">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Slated for version 0.0.3 (Initinal)
+    </p>
+    <p>
+      -Will need to add more items
+    </p>
+    <p>
+      -Need monsters to be able to drop items
+    </p>
+    <p>
+      -Need Plat, Gold, Silver, and Copper
+    </p>
+    <p>
+      --1000 gold - 1 plat
+    </p>
+    <p>
+      --1000 silver - 1 gold
+    </p>
+    <p>
+      --1000 copper - 1 silver
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1322624274067" HGAP="138" ID="ID_1302085601" MODIFIED="1322624320007" POSITION="left" TEXT="" VSHIFT="123"/>
+<node CREATED="1322587175783" HGAP="63" ID="ID_877343415" MODIFIED="1322624307707" POSITION="right" TEXT="Story" VSHIFT="94">
+<node CREATED="1322587216802" ID="ID_467274149" MODIFIED="1322624253675" TEXT="Jesse and Paul will build this"/>
+</node>
+</node>
+</map>
